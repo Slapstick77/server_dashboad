09f112be6b056994c84c309820fdfae0ccd51582
Slapstick77 <thomashansen6@gmail.com>
2025-10-30 20:53:03 -0500

    Update version to 1.3.1

--- webapp/version.py
+++ webapp/version.py
@@ -2,6 +2,6 @@
 Application version information
 """
 
-VERSION = "1.2.0"
-VERSION_DATE = "2025-10-07"
-VERSION_NAME = "Data Quality Release"
+VERSION = "1.3.1"
+VERSION_DATE = "2025-10-30"
+VERSION_NAME = "Metrics & TV Display Update"
